ca04b4d39207d1d5e3f28d19597576d4a9c1a614
Isuri Devlini Arachchige <arachchigeisuridevlini@gmail.com>
2021-08-17 14:42:06 +0530

    vehicle backend management

---
+++ guides-and-vehicles-backend/app/routes/vehicle.routes.js
@@ -0,0 +1,28 @@
+module.exports = app => {
+  const vehicles = require("../controllers/vehicle.controller");
+
+  var router = require("express").Router();
+
+  // Create a new TouristGuide
+  router.post("/", vehicles.create);
+
+  // Retrieve all TouristGuides
+  router.get("/", vehicles.findAll);
+
+  // Retrieve all published TouristGuides
+  router.get("/published", vehicles.findAllPublished);
+
+  // Retrieve a single TouristGuide with id
+  router.get("/:id", vehicles.findOne);
+
+  // Update a TouristGuide with id
+  router.put("/:id", vehicles.update);
+
+  // Delete a TouristGuide with id
+  router.delete("/:id", vehicles.delete);
+
+  // Create a new TouristGuide
+  router.delete("/", vehicles.deleteAll);
+
+  app.use("/api/vehicles", router);
+};
