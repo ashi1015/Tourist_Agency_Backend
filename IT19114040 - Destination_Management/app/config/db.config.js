b56333f1cec8a0d7a9935f0f1d3c91f6896267e8
Anuththara18 <64521257+Anuththara18@users.noreply.github.com>
2021-09-10 09:05:20 +0530

    config files committed

---
+++ IT19114040 - Destination_Management/app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  url: "mongodb+srv://spmUser:spmUser@spmproject.fuhvh.mongodb.net/smpProjectDatabase?retryWrites=true&w=majority"
+  //url: "mongodb://localhost:27017/tourist_agency"
+};
+
+//  url: "mongodb://localhost:27017/mern_db"
+//  url: "mongodb+srv://spmUser:spmUser@spmproject.fuhvh.mongodb.net/smpProjectDatabase?retryWrites=true&w=majority"
