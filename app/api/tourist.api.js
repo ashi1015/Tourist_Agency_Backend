022c0a5610ee4353f1a0863ccec84b23ffcdbd9d
sadunika-wasala <kasuni66wasala@gmail.com>
2021-09-21 17:44:38 +0530

    Backend tourists crud(add, view,update,delete) implementation done

---
+++ app/api/tourist.api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const touristsController = require("../controllers/tourist.controller");
+
+module.exports = function(){
+router.post('/register', touristsController.register); // handle any file using multer and save before passing controll to the registration controller
+router.post('/signin', touristsController.signin);
+router.get('/profile/:email', touristsController.getProfile);
+router.put('/update-profile/:email', touristsController.updateProfile)
+router.delete('/delete-profile/:email', touristsController.deleteProfile);
+
+return router
+}
+
+
