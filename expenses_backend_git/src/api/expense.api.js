566155ef17194dd782cc1a431bd54a5deb8b2b8e
sadunika-wasala <kasuni66wasala@gmail.com>
2021-09-26 08:36:00 +0530

    Update expenses.js

--- expenses_backend_git/src/api/expense.api.js
+++ expenses_backend_git/src/api/expense.api.js
@@ -6,8 +6,12 @@ const controller = require('../controllers/expense.controller');
 module.exports = function(){
     router.post('/', controller.createExpense);
     router.get('/view', controller.getAllExpenses);
-    router.put('/:id',controller.updateExpenseDetails);
-    router.delete('/:id', controller.deleteExpense);
+    router.get('/type/:type', controller.getExpensesByType);
+    router.put('/update-expenses/:id',controller.updateExpenseDetails);
+    router.delete('/delete-expense/:id', controller.deleteExpense);
+    router.get('/get-expense/:id',controller.getOneExpense);
+    router.get('/search-expenses/date/',controller.searchExpenses);
     return router
 
 }
+
