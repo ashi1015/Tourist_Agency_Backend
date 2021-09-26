855616461a9cd1d5b0a501154ac98fb36b5a2c64
sadunika-wasala <kasuni66wasala@gmail.com>
2021-09-26 08:30:50 +0530

    create path for delect tourist profile function

--- tourists_backend_git/src/api/tourist.api.js
+++ tourists_backend_git/src/api/tourist.api.js
@@ -6,7 +6,8 @@ module.exports = function(){
 router.post('/register', touristsController.register); // handle any file using multer and save before passing controll to the registration controller
 router.post('/signin', touristsController.signin);
 router.get('/profile/:email', touristsController.getProfile);
-router.put('/update-profile/:email', touristsController.updateProfile)
+router.put('/update-profile/:email', touristsController.updateProfile);
+router.delete('/delete-profile/:email', touristsController.deleteProfile);
 
 return router
 }
