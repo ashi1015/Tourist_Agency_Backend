1447fb7b544e24b90e95a113fdf6a1e87b93e327
sadunika-wasala <kasuni66wasala@gmail.com>
2021-09-26 08:37:21 +0530

    update expenses.cpntroller.js file

--- expenses_backend_git/src/controllers/expense.controller.js
+++ expenses_backend_git/src/controllers/expense.controller.js
@@ -4,7 +4,13 @@ const Expense = require('../models/expense.model');
 const createExpense = async (req, res) => {
     //check request body
     if (req.body) {
-        // console.log(expenseType);
+        newExpenseID = 1;
+        await Expense.find({}).sort({ expenseID: -1 }).limit(1)
+            .then(response => {
+                newExpenseID = response[0].expenseID + 1;
+            })
+            .catch(error => { });
+        req.body.expenseID = newExpenseID;
         const expense = new Expense(req.body); //create object for get data from the front end
         await expense.save() //save this data
             .then(data => {
@@ -18,7 +24,7 @@ const createExpense = async (req, res) => {
 
 //View all expenses
 const getAllExpenses = async (req, res) => {
-    await Expense.find({}).sort({year: -1}).sort({month: -1})
+    await Expense.find({}).sort({ date: -1 })//.sort({ month: -1 })
         .then(response => {
             res.status(200).send({ data: response });
         })
@@ -26,6 +32,20 @@ const getAllExpenses = async (req, res) => {
             res.status(500).send({ error: error.message });
         });
 }
+
+//get expenses by type
+const getExpensesByType = async (req, res) => {
+    await Expense.find({ expenseType: decodeURIComponent(req.params.type) }).sort({ date: -1 })//.sort({ month: -1 })
+        .then(response => {
+            res.status(200).send({ data: response });
+        })
+        .catch(error => {
+            res.status(500).send({ error: error.message });
+        });
+}
+
+
+
 
 //update Expense details
 const updateExpenseDetails = async (req, res) => {
@@ -51,11 +71,55 @@ const deleteExpense = async (req, res) => {
         });
 }
 
+//get expenses details by id
+const getOneExpense = async (req, res) => {
+    const id = req.params.id;
+
+    await Expense.findById(id)
+        .then((data) => {
+            if (!data)
+                res.status(404).send({ message: "Not found Expense with id " + id });
+            else res.send(data);
+        })
+        .catch((err) => {
+            res
+                .status(500)
+                .send({ message: "Error retrieving Expense with id=" + id });
+        });
+};
+
+//search Expenses between dates
+const searchExpenses = async (req, res) => {
+    const id = req.params.id;
+    var start = req.query.start;
+    var end = req.query.end;
+
+    await Expense.find({
+        date: {
+            $gte: new Date(start.split("-")[0], start.split("-")[1], 1),
+            $lt: new Date(end.split("-")[0], end.split("-")[1], 1)
+        }
+    })
+        .then((data) => {
+            if (!data)
+                res.status(404).send({ message: "Not found Expense with id " + id });
+            else res.send(data);
+        })
+        .catch((err) => {
+            res
+                .status(500)
+                .send({ message: "Error retrieving Expense with id=" + id });
+        });
+};
+
 
 //6-c-c export the function
 module.exports = {
     createExpense,
     getAllExpenses,
+    getExpensesByType,
     updateExpenseDetails,
-    deleteExpense
+    deleteExpense,
+    searchExpenses,
+    getOneExpense
 };
